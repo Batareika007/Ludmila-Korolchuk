9385e2b4eed42405d595ea33dd863e347d314b08
Batareika007 <batareika666@gmail.com>
2021-09-26 00:05:56 +0300

    carousel fail
    testing to create carousel, meanwhile fail

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
 // alert("hello");
 
+// swiper
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //   direction: 'vertical',
+    loop: true,
+
+    // direction: getDirection(),
+    // on: {
+    //     resize: function () {
+    //         swiper.changeDirection(getDirection());
+    //     },
+    // },
+    // If we need pagination
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     clicable: true
+    //   },
+
+    // Navigation arrows
+
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   },
+
+    // And if we need scrollbar
+    //   scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //   },
+});
+
+// function getDirection() {
+//         var windowWidth = window.innerWidth;
+//         var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+//         return direction;
+//       }
