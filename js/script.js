5e0407e1378cd3c7a0ccc4c24833e6b9b8b5356b
Batareika007 <batareika666@gmail.com>
2021-09-27 22:08:08 +0300

    certificates slider done
    used swiper slider

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
-// alert("hello");
+// alert("hello");    
 
 // swiper
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //   direction: 'vertical',
     loop: true,
-
-    // direction: getDirection(),
-    // on: {
-    //     resize: function () {
-    //         swiper.changeDirection(getDirection());
-    //     },
-    // },
-    // If we need pagination
-    //   pagination: {
-    //     el: '.swiper-pagination',
-    //     clicable: true
-    //   },
-
-    // Navigation arrows
-
-    //   navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //   },
-
-    // And if we need scrollbar
-    //   scrollbar: {
-    //     el: '.swiper-scrollbar',
-    //   },
+    spaceBetween: 32,
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    // autoHeight:true,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        // dynamicBullets:true,
+        // dynamicMainBullets:1,
+      },
+    breakpoints: {
+        // when window width is >= 320px
+        110: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            //   spaceBetween: 20
+        },
+        // when window width is >= 480px
+        540: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            //   spaceBetween: 30
+        },
+        // when window width is >= 640px
+        840: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            //   spaceBetween: 40
+        },
+        1100: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            //   spaceBetween: 40
+        }
+    }
+    
 });
-
-// function getDirection() {
-//         var windowWidth = window.innerWidth;
-//         var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-//         return direction;
-//       }
