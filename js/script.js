c5a71b8f27fa99ea5e1e2337a4a4437a616d9978
Batareika007 <batareika666@gmail.com>
2021-10-06 00:24:10 +0300

    styling responsive version
    styling responsive + add style to repair some content

--- js/script.js
+++ js/script.js
@@ -3,38 +3,36 @@
 // ----------------   swiper
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
+    // loop: true,
     spaceBetween: 32,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
+    // slidesPerView: 4,
+    // slidesPerGroup: 4,
     // autoHeight:true,
     grabCursor: true,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-        // dynamicBullets:true,
-        // dynamicMainBullets:1,
     },
     breakpoints: {
         // when window width is >= 320px
-        110: {
+        200: {
             slidesPerView: 1,
             slidesPerGroup: 1,
-            //   spaceBetween: 20
-        },
-        // when window width is >= 480px
-        540: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
             //   spaceBetween: 30
         },
+        // when window width is >= 480px
+        580: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+              spaceBetween: 30
+        },
         // when window width is >= 640px
-        840: {
+        900: {
             slidesPerView: 3,
             slidesPerGroup: 3,
             //   spaceBetween: 40
         },
-        1100: {
+        1140: {
             slidesPerView: 4,
             slidesPerGroup: 4,
             //   spaceBetween: 40
@@ -42,6 +40,14 @@ const swiper = new Swiper('.swiper', {
     }
 
 });
+
+const swiper2 = new Swiper('.swiper2', {
+    grabCursor:true,
+    loop:true,
+});
+
+
+
 
 // ------------------- hide header on scroll down
 
